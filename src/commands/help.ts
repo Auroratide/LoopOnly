88deccb483ffe31c251938e9b30d0a424b0858c6
Timothy Foster <tf.auroratide@gmail.com>
2021-11-11 16:18:47 -0600

    Better help text

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,10 +1,18 @@
 import type { Command } from './command'
 import type { CommandInteraction } from 'discord.js'
 
+const text = `I can do these things!
+
+\`/loop URL\`: Loop a song over and over again. The URL should be an audio file uploaded somewhere, and you should be in a voice channel.
+\`/pause\`: Pause the music
+\`/resume\`: Resume the music
+\`/disconnect\`: Disconnect me from the voice channel
+`
+
 export const Help: Command = {
     name: 'help',
     description: 'Learn what LoopOnly can do',
     execute: async (interaction: CommandInteraction) => {
-        await interaction.reply('I\'m not super helpful right now!')
+        await interaction.reply(text)
     },
 }
