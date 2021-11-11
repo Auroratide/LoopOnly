b7e1d805addcc804daaa70263c2482e0e7d6d96b
Timothy Foster <tf.auroratide@gmail.com>
2021-11-10 18:05:22 -0600

    Handle common http errors

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,8 +1,20 @@
 import * as stream from 'stream'
 import fetch from 'node-fetch'
+import { LoopOnlyError } from './error'
 
 export const audio = async (url: string): Promise<stream.Readable> => {
-    const res = await fetch(url)
+    const res = await fetch(url).catch(() => {
+        throw new LoopOnlyError(`Failed to fetch resource at ${url}`)
+    })
+
+    if (!res.ok) {
+        throw new LoopOnlyError(`Failed to fetch resource at ${url}`)
+    }
+
+    const contentType = res.headers.get('Content-Type') ?? ''
+    if (!/^audio\/.*/.test(contentType)) {
+        throw new LoopOnlyError(`Resource at ${url} is not a recognized audio file (${contentType})`)
+    }
 
     return stream.Readable.from(res.body!)
 }
